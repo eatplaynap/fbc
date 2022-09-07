4992e7fdf642e5d84b6da4d88397d78cac8d1871
eatplaynap <eatplaynap329@gmail.com>
2022-09-07 16:18:18 +0900

    テスト実装

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,8 +3,27 @@
 require_relative '../lib/fizzbuzz'
 
 RSpec.describe do
-  describe '#main' do
-    it '' do
+  describe '#run' do
+    it '3の倍数のときFizz、5の倍数のときBuzz、15の倍数のときFizzBuzz、それ以外のとき数字を返す' do
+      result = <<~TEXT
+        1
+        2
+        Fizz
+        4
+        Buzz
+        Fizz
+        7
+        8
+        Fizz
+        Buzz
+        11
+        Fizz
+        13
+        14
+        FizzBuzz
+      TEXT
+      fizzbuzz = FizzBuzz.new
+      expect { fizzbuzz.run(1..15) }.to output(result).to_stdout
     end
   end
 end
